6db831b5d436f9c1e7f2fb2bd0cf6ada5721b65c
slmarshall1 <slmarshall1@uclan.ac.uk>
2017-03-07 11:58:11 +0000

    removing array

--- XML_friends/JS/main2.js
+++ XML_friends/JS/main2.js
@@ -1,10 +1,10 @@
 var ourRequest = new XMLHttpRequest();
-ourRequest.open("GET","https://raw.githubusercontent.com/slmarshall1/xml_friends_ass_3/aec1631471b65fcf0cf6669af1a5e0faedbc4034/XML_friends/friends.xml");
+ourRequest.open("GET","https://raw.githubusercontent.com/slmarshall1/xml_friends_ass_3/6da9af2ade52d9493c0862c133c954e621597d04/XML_friends/friends.xml");
 ourRequest.onload = function (){
 
-	console.log(ourRequest.responseText);
+	//console.log(ourRequest.responseText);
 	var ourData = ourRequest.responseText;
-	;
+	console.log(ourData);
 };
 
 ourRequest.send();
